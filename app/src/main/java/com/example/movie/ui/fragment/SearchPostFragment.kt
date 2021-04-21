841626b25fdcf84765941ed66f61e0173ed9421b
KyungMin <2004ckck@naver.com>
2021-04-21 19:38:31 +0900

    [CREATE] Fragment.kt

---
+++ app/src/main/java/com/example/movie/ui/fragment/SearchPostFragment.kt
@@ -0,0 +1,19 @@
+package com.example.movie.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.movie.R
+
+
+class SearchPostFragment : Fragment() {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?)
+            : View? {
+        return inflater.inflate(R.layout.fragment_search_post, container, false)
+    }
+}
