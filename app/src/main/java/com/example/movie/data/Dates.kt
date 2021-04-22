7bfe12223d68a42f0bb6c2f2e78bb27979d9186f
KyungMin <2004ckck@naver.com>
2021-04-22 20:56:50 +0900

    [ADD] Movie Api dataClass

---
+++ app/src/main/java/com/example/movie/data/Dates.kt
@@ -0,0 +1,6 @@
+package com.example.movie.data
+
+data class Dates(
+    val maximum: String,
+    val minimum: String
+)
---
+++ app/src/main/java/com/example/movie/data/movie.kt
@@ -0,0 +1,9 @@
+package com.example.movie.data
+
+data class movie(
+    val dates: Dates,
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
