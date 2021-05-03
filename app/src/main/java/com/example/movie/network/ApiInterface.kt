6e554d2467548c83a248e73737e42f2819c7d407
KyungMin <2004ckck@naver.com>
2021-05-03 19:30:56 +0900

    [ADD] GET 추가 popular

--- app/src/main/java/com/example/movie/network/ApiInterface.kt
+++ app/src/main/java/com/example/movie/network/ApiInterface.kt
@@ -1,23 +1,29 @@
 package com.example.movie.network
 
-import com.example.movie.data.Result
+import com.example.movie.data.Movie
 import com.example.movie.untils.Constants.Companion.API_KEY
-import retrofit2.Response
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @GET("")
-    suspend fun getMovie(
-        @Query("KEY") KEY: String = API_KEY,
-        @Query("original_title") original_title: String,
-        @Query("overview") overview: String,
-        @Query("poster_path") poster_path: String,
-        @Query("backdrop_path") backdrop_path: String,
-        @Query("release_date") release_date: String
+    @GET("movie/upcoming")
+    fun getMovie(
+            @Query("api_key") apiKey: String = API_KEY,
+            @Query("page") page:Int,
+            @Query("language") language: String = "ko-KR"
 
-    ): Response<Result>
+    ): Call<Movie>
+
+    @GET("movie/popular")
+    fun getPopularMovie(
+            @Query("api_key") apiKey: String = API_KEY,
+            @Query("page") page:Int,
+            @Query("language") language: String = "ko-KR"
+
+    )  : Call<Movie>
+
 
 
 }
