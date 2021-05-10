1a372a698f249352d5c04585638d460cabcac72f
KyungMin <2004ckck@naver.com>
2021-05-10 18:18:16 +0900

    [UPDATE] 한글 제목

--- app/src/main/java/com/example/movie/ui/fragment/ExampleMovieFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/ExampleMovieFragment.kt
@@ -8,13 +8,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.movie.R
+import com.example.movie.ViewModel
+import com.example.movie.data.database.MovieEntity
 import com.example.movie.databinding.FragmentExampleMovieBinding
 import com.example.movie.untils.App
 import com.example.movie.untils.Constants.Companion.BASE_IMG_URL
 import com.example.movie.untils.Constants.Companion.TAG
+import kotlinx.coroutines.InternalCoroutinesApi
+
+@InternalCoroutinesApi
 
 class ExampleMovieFragment : Fragment() {
 
@@ -22,15 +28,18 @@ class ExampleMovieFragment : Fragment() {
     private var _binding: FragmentExampleMovieBinding? = null
     private val binding get() = _binding!!
     private val args by navArgs<ExampleMovieFragmentArgs>()
-    var i = 0
     private lateinit var like_img: ImageView
+
+    lateinit var contents: String
+//    lateinit var examText: String
+
 
     @SuppressLint("ResourceAsColor")
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
 
         _binding = FragmentExampleMovieBinding.inflate(inflater, container, false)
 
@@ -38,14 +47,23 @@ class ExampleMovieFragment : Fragment() {
 
 
         Glide.with(App.instance)
-            .load(BASE_IMG_URL + args.currentItem.poster_path)
-            .centerCrop()
-            .placeholder(R.drawable.test_post)
-            .into(binding.examImg)
+                .load(BASE_IMG_URL + args.currentItem.poster_path)
+                .centerCrop()
+                .placeholder(R.drawable.test_post)
+                .into(binding.examImg)
+
+
+        contents = binding.examContents.text.toString()
 
         like_img = binding.examLikeImg
-        binding.examContents.text = args.currentItem.overview
-        binding.examTitle.text = args.currentItem.original_title
+
+
+
+        contents = args.currentItem.overview
+        binding.examTitle.text = args.currentItem.title
+
+
+//        Log.d(TAG, "onCreateView: $examText")
 
 
 
@@ -58,7 +76,7 @@ class ExampleMovieFragment : Fragment() {
 
 
         }
-          
+
         return binding.root
     }
 
