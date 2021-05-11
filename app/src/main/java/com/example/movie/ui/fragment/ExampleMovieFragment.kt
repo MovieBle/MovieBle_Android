de7406e070239ab57a8d8653fc1bb488bea23f1a
KyungMin <2004ckck@naver.com>
2021-05-11 21:04:31 +0900

    [UPDATE] Type 변경

--- app/src/main/java/com/example/movie/ui/fragment/ExampleMovieFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/ExampleMovieFragment.kt
@@ -6,33 +6,40 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.movie.R
-import com.example.movie.ViewModel
+import com.example.movie.data.Result
 import com.example.movie.data.database.MovieEntity
 import com.example.movie.databinding.FragmentExampleMovieBinding
 import com.example.movie.untils.App
 import com.example.movie.untils.Constants.Companion.BASE_IMG_URL
 import com.example.movie.untils.Constants.Companion.TAG
+import com.example.movie.viewmodels.DatabaseViewModel
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @InternalCoroutinesApi
 
 class ExampleMovieFragment : Fragment() {
-
+    var i = 0
 
     private var _binding: FragmentExampleMovieBinding? = null
     private val binding get() = _binding!!
     private val args by navArgs<ExampleMovieFragmentArgs>()
-    private lateinit var like_img: ImageView
 
-    lateinit var contents: String
-//    lateinit var examText: String
+    private val databaseViewModel: DatabaseViewModel by viewModels()
+    private lateinit var like_img: ImageButton
 
+    private lateinit var contents: TextView
+    private lateinit var examText: TextView
+
+
+    var likeList = mutableListOf<Result>()
 
     @SuppressLint("ResourceAsColor")
     override fun onCreateView(
@@ -53,36 +60,68 @@ class ExampleMovieFragment : Fragment() {
                 .into(binding.examImg)
 
 
-        contents = binding.examContents.text.toString()
-
+        contents = binding.examContents
+        examText = binding.examTitle
         like_img = binding.examLikeImg
 
+        like_img.setBackgroundColor(R.color.black)
+
+        contents.text = args.currentItem.overview
+        examText.text = args.currentItem.title
 
 
-        contents = args.currentItem.overview
-        binding.examTitle.text = args.currentItem.title
-
-
-//        Log.d(TAG, "onCreateView: $examText")
-
+        Log.d(TAG, "onCreateView: $examText")
 
 
         like_img.setOnClickListener {
 
+            if (i == 0) {
+                like_img.setBackgroundColor(R.color.red)
 
-            like_img.setBackgroundColor(R.color.black)
-
-            insertMovie()
-
+                insertMovie()
+                i++
+            } else {
+                like_img.setBackgroundColor(R.color.black)
+                deleteMovie()
+                i--
+            }
 
         }
 
         return binding.root
     }
 
+
     private fun insertMovie() {
+        val movieData = MovieEntity(
+
+                BASE_IMG_URL + args.currentItem.poster_path,
+
+                args.currentItem.title
+
+        )
+        databaseViewModel.insertData(movieData)
 
 
+        Toast.makeText(requireContext(), "포스트가 추가됩니다.", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "insertMovie: ${BASE_IMG_URL + args.currentItem.poster_path}, ${args.currentItem.title}")
+
+    }
+
+    private fun deleteMovie() {
+        val movieData = MovieEntity(
+
+
+
+                BASE_IMG_URL + args.currentItem.poster_path,
+
+                args.currentItem.title
+        )
+
+        databaseViewModel.deleteData(movieData)
+
+        Toast.makeText(requireContext(), "포스트가 지워집니다.", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "deleteMovie: $movieData")
     }
 
 
