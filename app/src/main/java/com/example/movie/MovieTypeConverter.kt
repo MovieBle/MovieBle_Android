4b218d353053eaf332a4655f6ebfa4cd4ad94f5f
KyungMin <2004ckck@naver.com>
2021-05-13 19:34:41 +0900

    [UPDATE] type

--- app/src/main/java/com/example/movie/MovieTypeConverter.kt
+++ app/src/main/java/com/example/movie/MovieTypeConverter.kt
@@ -2,22 +2,25 @@ package com.example.movie
 
 import androidx.room.TypeConverter
 import com.example.movie.data.Movie
+import com.example.movie.data.Result
+import com.example.movie.data.database.MovieEntity
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
+// json 을 저장할려면 converter 로 String 값으로 바꿔야함
 class MovieTypeConverter {
     var gson = Gson()
 
     @TypeConverter
-    fun foodRecipeToString(movie: Movie): String {
-
-        return gson.toJson(movie)
-    }
-
-    @TypeConverter
-    fun stringToFoodRecipe(data: String): Movie {
-
-        val listType = object : TypeToken<Movie>() {}.type
+    fun stringToResult(data: String): Result {
+        val listType = object : TypeToken<Result>() {}.type
         return gson.fromJson(data, listType)
     }
+
+
+    @TypeConverter
+    fun resultToString(result: Result): String {
+        return gson.toJson(result)
+    }
+
 }
