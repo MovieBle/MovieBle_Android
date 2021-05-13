8aaea45155eeb9834a4e106e5d81aef498825b8d
KyungMin <2004ckck@naver.com>
2021-05-13 19:34:02 +0900

    [CREATE] DiffUtil

---
+++ app/src/main/java/com/example/movie/untils/MovieDiffUtil.kt
@@ -0,0 +1,30 @@
+package com.example.movie.untils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.movie.data.Result
+
+class MovieDiffUtil(
+        private val oldList: List<Result>,
+        private val newList: List<Result>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        // 함수목록 가져오기
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        // 위치가 같는지
+        return oldList[oldItemPosition] === newList[newItemPosition]
+
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] === newList[newItemPosition]
+
+    }
+
+}
