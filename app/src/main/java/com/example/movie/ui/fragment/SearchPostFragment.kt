ad8b79ceedaeeb6c61ab92572478fc44408ed9cd
KyungMin <2004ckck@naver.com>
2021-05-17 22:49:58 +0900

    [ADD] filter TextChange

--- app/src/main/java/com/example/movie/ui/fragment/SearchPostFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/SearchPostFragment.kt
@@ -2,21 +2,20 @@ package com.example.movie.ui.fragment
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.movie.untils.MovieCase
 import com.example.movie.R
 import com.example.movie.adapters.MovieListAdapter
 import com.example.movie.data.Movie
 import com.example.movie.data.Result
-import com.example.movie.databinding.FragmentMoiveListBinding
 import com.example.movie.databinding.FragmentSearchPostBinding
 import com.example.movie.network.RecentBuilder
-import com.example.movie.untils.Constants
+import com.example.movie.untils.Constants.Companion.TAG
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -27,38 +26,83 @@ class SearchPostFragment : Fragment() {
 
     private var _binding: FragmentSearchPostBinding? = null
     private val binding get() = _binding!!
+    var mSearchAdapter: MovieListAdapter? = null
 
+    private val movieList: List<Result>? = null
     var movie_recycler: RecyclerView? = null
+    private val viewmodel: com.example.movie.viewmodels.ViewModel by viewModels()
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?)
-            : View? {
+            : View {
+
+        mSearchAdapter?.notifyDataSetChanged()
+        movieList?.let { mSearchAdapter?.setData(it) }
+        getNowPlayingMovies()
         _binding = FragmentSearchPostBinding.inflate(inflater, container, false)
-        getMovie()
-        movie_recycler=binding.searchRecycler
+
+        movie_recycler = binding.searchRecycler
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+
     private fun setRecentAdapter(movieList: List<Result>) {
 
+        mSearchAdapter = MovieListAdapter(movieList, MovieCase.SEARCH_LIST_VIEW, requireContext())
 
-
-        val mAdapter = MovieListAdapter(movieList, requireContext())
-        movie_recycler?.adapter = mAdapter
+        movie_recycler?.adapter = mSearchAdapter
         movie_recycler?.layoutManager =
-                GridLayoutManager(requireContext(), 2,GridLayoutManager.VERTICAL, false,)
+                GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
         movie_recycler?.setHasFixedSize(false)
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_search, menu)
+        val searchView = menu.findItem(R.id.menu_action_search).actionView as SearchView
+
+        searchView.queryHint = getString(R.string.search_view_hint)
 
 
 
 
-    fun getMovie(page: Int = 1): Boolean {
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    Log.d(TAG, "onQueryTextSubmit: ")
+                    mSearchAdapter?.filter?.filter(query)
+                    movieList?.let { mSearchAdapter?.setData(it) }
 
-        RecentBuilder.service.getMovie(page = page).enqueue(object : Callback<Movie> {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    Log.d(TAG, "onQueryTextChange: ")
+                    mSearchAdapter?.filter?.filter(newText)
+                    movieList?.let { mSearchAdapter?.setData(it) }
+
+                    return true
+                }
+
+            })
+
+
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.menu_action_search ->
+                true
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+        private fun getNowPlayingMovies(page: Int = 1): Boolean {
+
+        RecentBuilder.service.getNowPlayingMovies(page = page).enqueue(object : Callback<Movie> {
             override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
 
 
@@ -68,9 +112,10 @@ class SearchPostFragment : Fragment() {
                     val body = response.body()
                     body?.let {
                         setRecentAdapter(it.results)
-                        Log.d(Constants.TAG, "onResponse: ${it.results}")
+                        Log.d(TAG, "onResponse: ${it.results}")
 
                     }
+
 
                 }
             }
@@ -83,4 +128,6 @@ class SearchPostFragment : Fragment() {
         return false
 
     }
+
+
 }
