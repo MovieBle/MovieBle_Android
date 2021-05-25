c2fcf4eb4d05b5b8e7f2880849471523d11106f3
KyungMin <2004ckck@naver.com>
2021-05-25 22:32:22 +0900

    [ADD] DELETE Fun

--- app/src/main/java/com/example/movie/data/database/MovieDao.kt
+++ app/src/main/java/com/example/movie/data/database/MovieDao.kt
@@ -1,18 +1,20 @@
 package com.example.movie.data.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.lifecycle.LiveData
+import androidx.room.*
 import com.example.movie.data.Movie
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface MovieDao {
 
-    @Insert
-   suspend fun insertMovie(movie: Movie)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie: MovieEntity)
 
+    @Delete
+    suspend fun deleteData(movie: MovieEntity)
 
     @Query("SELECT * FROM like_movie_table ORDER BY id ASC")
-    fun readMovie() :List<MovieEntity>
+    fun getAllData() : LiveData<List<MovieEntity>>
 }
