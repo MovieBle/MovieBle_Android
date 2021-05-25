c54cae0087ea706ad887a5000da2b69db11aec36
KyungMin <2004ckck@naver.com>
2021-05-25 20:01:07 +0900

    [ADD] readDatabase

--- app/src/main/java/com/example/movie/repositorys/DatabaseRepository.kt
+++ app/src/main/java/com/example/movie/repositorys/DatabaseRepository.kt
@@ -1,15 +1,20 @@
-package com.example.movie
+package com.example.movie.repositorys
 
 import androidx.lifecycle.LiveData
 import com.example.movie.data.Movie
 import com.example.movie.data.database.MovieDao
 import com.example.movie.data.database.MovieEntity
+import com.example.movie.network.ApiInterface
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
 
-class Repository(private val movieDao: MovieDao) {
+class DatabaseRepository(private val movieDao: MovieDao) {
 
     val getAllData: LiveData<List<MovieEntity>> = movieDao.getAllData()
 
-
+    fun readDatabase(): LiveData<List<MovieEntity>> {
+        return movieDao.getAllData()
+    }
     suspend fun insertData(movie: MovieEntity) {
         movieDao.insertMovie(movie)
     }
