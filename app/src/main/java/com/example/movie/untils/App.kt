f9b5ca4a0c0f9deb1435c456b84ff6bce24bcd61
KyungMin <2004ckck@naver.com>
2021-05-31 23:28:45 +0900

    [ADD] Dagger Hilt @HiltAndroidApp 어노테이션 추가

--- app/src/main/java/com/example/movie/untils/App.kt
+++ app/src/main/java/com/example/movie/untils/App.kt
@@ -1,7 +1,13 @@
 package com.example.movie.untils
 
 import android.app.Application
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+
+@HiltAndroidApp
 class App : Application() {
     companion object {
         lateinit var instance: App
