4bdce2bc58362e1be7a4906654cad946aa66f27f
KyungMin <2004ckck@naver.com>
2021-05-31 23:35:28 +0900

    [ADD]@HiltViewModel, @Inject 어노테이션 추가 [RENAME] repository 변수 이름 변경

--- app/src/main/java/com/example/movie/viewmodels/NetworkViewModel.kt
+++ app/src/main/java/com/example/movie/viewmodels/NetworkViewModel.kt
@@ -7,51 +7,56 @@ import androidx.lifecycle.LiveData
 import com.example.movie.data.Movie
 import com.example.movie.repositorys.NetworkRepository
 import com.example.movie.untils.Constants.Companion.TAG
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.InternalCoroutinesApi
+import javax.inject.Inject
 
-class NetworkViewModel(application: Application) : AndroidViewModel(application) {
+@HiltViewModel
+class NetworkViewModel @Inject constructor(
 
+    application: Application,
+    private val networkRepository: NetworkRepository
+) : AndroidViewModel(application) {
 
-    private val baeminRepository = NetworkRepository()
 
     // LiveData 가 아닌 MutableLiveData 를 쓴 이유
     // LiveData 는 읽기전용으로 값의 변경을 못하지만 MutableLiveData는 get/set이 가능하다. 그래서 데이터 변경이 잦는
     // 네트워크는 MutableLiveData를 쓰는게 합리적이다.
     private val result: LiveData<Movie>
-        get() = baeminRepository.getMovie
+        get() = networkRepository.getMovie
 
     private val resultPopular: LiveData<Movie>
-        get() = baeminRepository.getPopular
+        get() = networkRepository.getPopular
 
     private val resultTop: LiveData<Movie>
-        get() = baeminRepository.getTop
+        get() = networkRepository.getTop
 
     private val resultDiscover: LiveData<Movie>
-        get() = baeminRepository.getDiscover
+        get() = networkRepository.getDiscover
 
     private val resultRecent: LiveData<Movie>
-        get() = baeminRepository.getRecent
+        get() = networkRepository.getRecent
+
 
     fun getNowPlayingMovies(page: Int) {
 
         Log.d(TAG, "getNowPlayingMovies: ")
-        baeminRepository.getNowPlayingMovies(page)
+        networkRepository.getNowPlayingMovies(page)
 
     }
+
     fun getTopRatedMovies(page: Int) {
 
         Log.d(TAG, "getTopRatedMovies: ")
-        baeminRepository.getTopRatedMovies(page)
+        networkRepository.getTopRatedMovies(page)
 
     }
-
-
 
 
     fun getDiscoverMovies(page: Int) {
 
         Log.d(TAG, "getDiscoverMovies: ")
-        baeminRepository.getDiscoverMovies(page)
+        networkRepository.getDiscoverMovies(page)
 
     }
 
@@ -66,7 +71,7 @@ class NetworkViewModel(application: Application) : AndroidViewModel(application)
 
         Log.d(TAG, "getMovie: ")
 
-        baeminRepository.getRecentMovie(page)
+        networkRepository.getRecentMovie(page)
 
     }
 
@@ -74,7 +79,7 @@ class NetworkViewModel(application: Application) : AndroidViewModel(application)
     fun getPopularMovies(page: Int) {
 
         Log.d(TAG, "getPopularMovies: ")
-        baeminRepository.getPopularMovies(page)
+        networkRepository.getPopularMovies(page)
 
     }
 
@@ -85,12 +90,15 @@ class NetworkViewModel(application: Application) : AndroidViewModel(application)
     fun getAllPopular(): LiveData<Movie> {
         return resultPopular
     }
+
     fun getAllTop(): LiveData<Movie> {
         return resultTop
     }
+
     fun grtAllDiscover(): LiveData<Movie> {
         return resultDiscover
     }
+
     fun getAllRecent(): LiveData<Movie> {
         return resultRecent
     }
