a722f886e13059c932d5047faa3c0ca55b93d38b
KyungMin <2004ckck@naver.com>
2021-06-08 23:22:47 +0900

    [DELETE] 폴더 이동 중 삭제

--- app/src/main/java/com/example/movie/data/Movie.kt
+++
@@ -1,9 +0,0 @@
-package com.example.movie.data
-
-data class  Movie(
-    val dates: Dates,
-    val page: Int,
-    val results: List<Result>,
-    val total_pages: Int,
-    val total_results: Int
-)
