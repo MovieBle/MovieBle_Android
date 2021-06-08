9459efbbc11b1c82dd9495df5e0aeecfe05922a9
KyungMin <2004ckck@naver.com>
2021-06-08 23:23:39 +0900

    [DELETE] 널참조 제거

--- app/src/main/java/com/example/movie/ui/fragment/SearchPostFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/SearchPostFragment.kt
@@ -1,38 +1,52 @@
 package com.example.movie.ui.fragment
 
+import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movie.R
 import com.example.movie.adapters.SearchViewAdapter
-import com.example.movie.data.Result
+import com.example.movie.models.Result
 import com.example.movie.databinding.FragmentSearchPostBinding
+import com.example.movie.observeOnce
 import com.example.movie.untils.Constants.Companion.TAG
-import com.example.movie.viewmodels.NetworkViewModel
+import com.example.movie.untils.NetworkResult
+import com.example.movie.viewmodels.DatabaseViewModel
+import com.example.movie.viewmodels.QueryViewModel
+import com.todkars.shimmer.ShimmerRecyclerView
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 
 
+@AndroidEntryPoint
 class SearchPostFragment : Fragment() {
 
 
     private var _binding: FragmentSearchPostBinding? = null
     private val binding get() = _binding!!
 
-
-    var movie_recycler: RecyclerView? = null
-    private val networkViewModel: NetworkViewModel by viewModels()
+    @InternalCoroutinesApi
+    private  val databaseViewModel: DatabaseViewModel by viewModels()
+    private  val queryViewModel: QueryViewModel by viewModels()
+    var movie_recycler: ShimmerRecyclerView? = null
 
     private var page = 1
     var movieList = mutableListOf<Result>()
     lateinit var mSearchAdapter: SearchViewAdapter
 
 
+    @InternalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -45,14 +59,6 @@ class SearchPostFragment : Fragment() {
         mSearchAdapter = SearchViewAdapter(movieList, requireContext())
 
 
-        //실행안됨 error
-        networkViewModel.getAll().observe(viewLifecycleOwner, Observer {
-
-            Log.d(TAG, "SearchPostFragment - onCreateView() called")
-            mSearchAdapter.setList(it.results as MutableList<Result>)
-            mSearchAdapter.notifyItemRangeInserted((page - 1) * 19, 19)
-        })
-
 
         movie_recycler = binding.searchRecycler
 
@@ -61,9 +67,7 @@ class SearchPostFragment : Fragment() {
         recyclerViewPaging()
 
         setRecentAdapter(movieList)
-        //getNowPlayingMovies(page,requireContext())
 
-        networkViewModel.getNowPlayingMovies(page)
 
 
         binding.refreshLayout.setOnRefreshListener {
@@ -74,15 +78,79 @@ class SearchPostFragment : Fragment() {
 
         }
 
-
-
+        databaseViewModel.getPopularMovie(queryViewModel.getQuery())
+        readDataBase()
         return binding.root
 
     }
+    @InternalCoroutinesApi
+    private fun readDataBase() {
+
+        // 비동기 생성
+        lifecycleScope.launch {
+            databaseViewModel.getAllData.observeOnce(viewLifecycleOwner, { database ->
+                Log.d(TAG, "readDataBase: observer")
+                // 비어있지 않다면
+                if (database.isNotEmpty()) {
+
+                    Log.d(ContentValues.TAG, "readDataBase: isNotEmpty ")
+                    mSearchAdapter.setData(database[0].movie)
+
+
+                } else {
+                    Log.d(TAG, "readDataBase: Empty")
+                    // 비어있으면
+                    requestApiData()
+                }
+
+            })
+        }
+    }
+
+    @InternalCoroutinesApi
+    private fun requestApiData() {
+
+
+
+        databaseViewModel.getAllMovie.observe(viewLifecycleOwner, { response ->
+
+            Log.d(TAG, "requestApiData: ")
+//           // response 상태에 따라 로직 나누기
+            when (response) {
+
+                is NetworkResult.Success -> {
+                    //데이터가 들어오면
+                    Log.d(TAG, "requestApiData: success")
+                    hideShimmerEffect()
+                    response.data?.let {
+                        mSearchAdapter.setData(((it)))
+
+                    }
+                }
+                is NetworkResult.Error -> {
+                    Log.d(TAG, "requestApiData: error")
+                    hideShimmerEffect()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                is NetworkResult.Loading -> {
+                    Log.d(TAG, "requestApiData: loading")
+                    showShimmerEffect()
+                }
+
+            }
+        })
+    }
+
+
 
     fun recyclerViewPaging() {
 
-
+        val upBtn = binding.upBtn
         movie_recycler?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
@@ -91,16 +159,44 @@ class SearchPostFragment : Fragment() {
                     (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
                 val itemTotalCount = recyclerView.adapter!!.itemCount - 1
 
+                Log.d(TAG, " position: $lastVisibleItemPosition")
+                Log.d(TAG, "onScrolled : Page: $page")
+
+                if (lastVisibleItemPosition > 19) {
+
+
+                    upBtn.visibility = View.VISIBLE
+                    binding.upBtn.setOnClickListener() {
+
+                        movie_recycler!!.scrollToPosition(0)
+
+                    }
+                } else binding.upBtn.visibility = View.GONE
+
                 //recyclerView 마지막일때
                 if (!movie_recycler!!.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount) {
                     //  mSearchAdapter.deleteLoading()
-                    Log.d(TAG, "onScrolled Position: $lastVisibleItemPosition")
-                    networkViewModel.getNowPlayingMovies(++page)
-                    Log.d(TAG, "onScrolled : Page: $page")
+                    Log.d(TAG, "마지막 position: $lastVisibleItemPosition")
+
+
+//                    networkViewModel.getNowPlayingMovies(++page)
+
 
                 }
+
+
             }
         })
+    }
+    private fun showShimmerEffect() {
+
+        movie_recycler?.showShimmer()
+
+    }
+
+    private fun hideShimmerEffect() {
+        movie_recycler?.hideShimmer()
+
     }
 
 
@@ -124,11 +220,11 @@ class SearchPostFragment : Fragment() {
         searchView.queryHint = getString(R.string.search_view_hint)
 
 
-    //recyclerVIew filter
+        //recyclerVIew filter
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 Log.d(TAG, "onQueryTextSubmit: ")
-                mSearchAdapter.filter?.filter(query)
+                mSearchAdapter.filter.filter(query)
                 movieList.let { mSearchAdapter?.setFilterData(it) }
 
                 return false
@@ -136,8 +232,8 @@ class SearchPostFragment : Fragment() {
 
             override fun onQueryTextChange(newText: String?): Boolean {
                 Log.d(TAG, "onQueryTextChange: ")
-                mSearchAdapter.filter?.filter(newText)
-                movieList.let { mSearchAdapter?.setFilterData(it) }
+                mSearchAdapter.filter.filter(newText)
+                movieList.let { mSearchAdapter.setFilterData(it) }
 
                 return true
             }
