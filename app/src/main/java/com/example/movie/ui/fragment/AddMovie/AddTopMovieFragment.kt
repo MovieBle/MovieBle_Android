f5bafc04d06b0f3335bd24f3c81e20e69481cf9b
KyungMin <2004ckck@naver.com>
2021-06-08 11:57:14 +0900

    [UPDATE] retrofit2 call에서 response으로 바꾸는 과정중 필요없는 코드 제거

--- app/src/main/java/com/example/movie/ui/fragment/AddMovie/AddTopMovieFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/AddMovie/AddTopMovieFragment.kt
@@ -11,11 +11,10 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movie.adapters.MovieListAdapter
-import com.example.movie.databinding.FragmentAddRecentMovieBinding
 import com.example.movie.databinding.FragmentTopAddMovieBinding
+import com.example.movie.models.Result
 import com.example.movie.untils.Constants
 import com.example.movie.untils.MovieCase
-import com.example.movie.viewmodels.NetworkViewModel
 
 class AddTopMovieFragment : Fragment() {
 
@@ -24,14 +23,13 @@ class AddTopMovieFragment : Fragment() {
 
     private val listAdapter: MovieListAdapter by lazy {
         MovieListAdapter(
-            MovieCase.MOVIE_TOP
+            movieList,     MovieCase.MOVIE_TOP
 
         )
     }
     var page=1
-    private val networkViewModel: NetworkViewModel by viewModels()
     private var top_recycler: RecyclerView? = null
-
+    var movieList = emptyList<Result>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -44,13 +42,6 @@ class AddTopMovieFragment : Fragment() {
         top_recycler = binding.topRecycler
         setTopAdapter()
 
-        networkViewModel.getTopRatedMovies(page)
-
-        networkViewModel.getAllTop().observe(viewLifecycleOwner, Observer {
-            listAdapter.setData(it.results)
-            listAdapter.notifyItemRangeInserted((page - 1) * 19, 19)
-
-        })
         recyclerViewAddPlus()
         return binding.root
     }
@@ -68,7 +59,6 @@ class AddTopMovieFragment : Fragment() {
                 if (!top_recycler!!.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount) {
                     //  mSearchAdapter.deleteLoading()
                     Log.d(Constants.TAG, "onScrolled Position: $lastVisibleItemPosition")
-                    networkViewModel.getTopRatedMovies(++page)
                     Log.d(Constants.TAG, "onScrolled : Page: $page")
 
                 }
