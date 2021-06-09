458e535a5a788a8db2804892b921fac86af33faf
KyungMin <2004ckck@naver.com>
2021-06-09 21:12:27 +0900

    [UPDATE] import 패키지 변경

--- app/src/main/java/com/example/movie/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/movie/ui/activity/LoginActivity.kt
@@ -3,13 +3,14 @@ package com.example.movie.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.movie.FragmentMainActivity
 import com.example.movie.databinding.ActivityMainBinding
+import kotlinx.coroutines.InternalCoroutinesApi
 
 class LoginActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
