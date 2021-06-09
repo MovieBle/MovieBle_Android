b238cb7345b18221ea36888a49e7b1010faba48f
KyungMin <2004ckck@naver.com>
2021-06-09 21:10:28 +0900

    [UPDATE] 툴바 색깔 변ㅂ경

--- app/src/main/java/com/example/movie/ui/activity/FragmentMainActivity.kt
+++ app/src/main/java/com/example/movie/ui/activity/FragmentMainActivity.kt
@@ -1,32 +1,23 @@
 package com.example.movie.ui.activity
 
-import android.app.Fragment
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
 import android.view.View
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
 import com.example.movie.R
-import com.example.movie.adapters.LikeViewAdapter
-import com.example.movie.data.database.MovieEntity
 import com.example.movie.databinding.ActivityNaviMainBinding
-import com.example.movie.ui.fragment.ExampleMovieFragment
 import com.example.movie.ui.fragment.MovieListFragment
 import com.example.movie.ui.fragment.ProfileFragment
 import com.example.movie.ui.fragment.SearchPostFragment
-import com.example.movie.untils.Constants
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.InternalCoroutinesApi
 
 
 @AndroidEntryPoint
@@ -40,8 +31,6 @@ class FragmentMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         navi = binding.bottomNavigationView
-
-
         //NavController 생성
         navController = findNavController(R.id.navHostFragment)
         //앱 바 구성성
@@ -73,15 +62,26 @@ class FragmentMainActivity : AppCompatActivity() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             // fragment id 가 아닐 시 bottom navigation 안뜸
             if (destination.id == R.id.profileFragment || destination.id == R.id.movieListFragment || destination.id == R.id.searchPostFragment) {
+                setToolbarColor(R.color.black)
                 navi.visibility = View.VISIBLE
             } else {
+                setToolbarColor(R.color.green)
                 navi.visibility = View.GONE
             }
         }
     }
 
 
-
+    private fun setToolbarColor(colorId: Int) {
+        supportActionBar!!.setBackgroundDrawable(
+            ColorDrawable(
+                ContextCompat.getColor(
+                    this,
+                    colorId
+                )
+            )
+        )
+    }
 
 
 }
