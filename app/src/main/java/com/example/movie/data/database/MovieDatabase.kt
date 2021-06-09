165017647e036e8a77d3f5887d086b75b2f23dd0
KyungMin <2004ckck@naver.com>
2021-06-09 21:14:12 +0900

    [UPDATE] dataBase version 14로 변경

--- app/src/main/java/com/example/movie/data/database/MovieDatabase.kt
+++ app/src/main/java/com/example/movie/data/database/MovieDatabase.kt
@@ -1,12 +1,12 @@
 package com.example.movie.data.database
 
 import androidx.room.*
-import com.example.movie.data.database.entities.MovieEntity
 import com.example.movie.data.database.entities.MovieLikeEntity
+import com.example.movie.data.database.entities.movie.*
 
 @Database(
-    entities=[MovieEntity::class, MovieLikeEntity::class],
-    version=12   ,
+    entities = [MovieEntity::class, MovieLikeEntity::class, MovieDiscoverEntity::class, MoviePopularEntity::class, MovieRecentEntity::class,MovieListEntity::class],
+    version = 14,
     exportSchema = false
 
 )
