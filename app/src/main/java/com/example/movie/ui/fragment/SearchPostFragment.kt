c0979855b6f2173a83e643b976a02065347e9ce5
KyungMin <2004ckck@naver.com>
2021-06-10 19:37:02 +0900

    [ADD] paging 추가

--- app/src/main/java/com/example/movie/ui/fragment/SearchPostFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/SearchPostFragment.kt
@@ -1,6 +1,5 @@
 package com.example.movie.ui.fragment
 
-import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
 import android.view.*
@@ -9,15 +8,14 @@ import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movie.R
 import com.example.movie.adapters.SearchViewAdapter
-import com.example.movie.models.Result
 import com.example.movie.databinding.FragmentSearchPostBinding
+import com.example.movie.models.Result
 import com.example.movie.observeOnce
 import com.example.movie.untils.Constants.Companion.TAG
 import com.example.movie.untils.NetworkResult
@@ -37,8 +35,8 @@ class SearchPostFragment : Fragment() {
     private val binding get() = _binding!!
 
     @InternalCoroutinesApi
-    private  val databaseViewModel: DatabaseViewModel by viewModels()
-    private  val queryViewModel: QueryViewModel by viewModels()
+    private val databaseViewModel: DatabaseViewModel by viewModels()
+    private val queryViewModel: QueryViewModel by viewModels()
     var movie_recycler: ShimmerRecyclerView? = null
 
     private var page = 1
@@ -56,49 +54,91 @@ class SearchPostFragment : Fragment() {
 
 
         _binding = FragmentSearchPostBinding.inflate(inflater, container, false)
-        mSearchAdapter = SearchViewAdapter(movieList, requireContext())
-
 
 
         movie_recycler = binding.searchRecycler
 
-        setHasOptionsMenu(true)
+        requestApiData()
 
         recyclerViewPaging()
 
         setRecentAdapter(movieList)
 
 
-
         binding.refreshLayout.setOnRefreshListener {
-
             mSearchAdapter.notifyDataSetChanged() // 새로고침 하고
+
+            readDataBase(page)
+
             binding.refreshLayout.isRefreshing = false // 새로고침을 완료하면 아이콘을 없앤다.
 
 
         }
 
-        databaseViewModel.getPopularMovie(queryViewModel.getQuery())
-        readDataBase()
+        databaseViewModel.getAllDiscoverData.observe(viewLifecycleOwner, Observer {
+            mSearchAdapter.notifyItemRangeInserted((page - 1) * 19, 19)
+
+        })
+
+
+        Log.d(TAG, "onCreateView: $page")
+        readDataBase(page++)
+
+        setHasOptionsMenu(true)
         return binding.root
 
     }
+
     @InternalCoroutinesApi
-    private fun readDataBase() {
+    fun recyclerViewPaging() {
+
+        val upBtn = binding.upBtn
+        movie_recycler?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+
+                val lastVisibleItemPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
+                val itemTotalCount = recyclerView.adapter!!.itemCount - 1
+
+                Log.d(TAG, "position: $lastVisibleItemPosition")
+                if (lastVisibleItemPosition > 19) {
+
+
+                    upBtn.visibility = View.VISIBLE
+                    binding.upBtn.setOnClickListener() {
+
+                        movie_recycler!!.scrollToPosition(0)
+
+                    }
+                } else binding.upBtn.visibility = View.GONE
+
+                //recyclerView 마지막일때
+                if (!movie_recycler!!.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount) {
+
+                    movie_recycler?.showShimmer()
+                    readDataBase(++page)
+                }
+
+
+            }
+        })
+    }
+
+
+    @InternalCoroutinesApi
+    private fun readDataBase(page: Int) {
 
         // 비동기 생성
         lifecycleScope.launch {
-            databaseViewModel.getAllData.observeOnce(viewLifecycleOwner, { database ->
-                Log.d(TAG, "readDataBase: observer")
-                // 비어있지 않다면
+            databaseViewModel.getAllDiscoverData.observeOnce(viewLifecycleOwner, { database ->
+                Log.d(TAG, "addPage: $page")
+                databaseViewModel.getDiscoverMovie(queryViewModel.getQuery(), page)
+
                 if (database.isNotEmpty()) {
-
-                    Log.d(ContentValues.TAG, "readDataBase: isNotEmpty ")
                     mSearchAdapter.setData(database[0].movie)
-
-
                 } else {
-                    Log.d(TAG, "readDataBase: Empty")
                     // 비어있으면
                     requestApiData()
                 }
@@ -107,9 +147,9 @@ class SearchPostFragment : Fragment() {
         }
     }
 
+
     @InternalCoroutinesApi
     private fun requestApiData() {
-
 
 
         databaseViewModel.getAllMovie.observe(viewLifecycleOwner, { response ->
@@ -124,8 +164,9 @@ class SearchPostFragment : Fragment() {
                     hideShimmerEffect()
                     response.data?.let {
                         mSearchAdapter.setData(((it)))
-
                     }
+
+
                 }
                 is NetworkResult.Error -> {
                     Log.d(TAG, "requestApiData: error")
@@ -135,64 +176,19 @@ class SearchPostFragment : Fragment() {
                         response.message.toString(),
                         Toast.LENGTH_SHORT
                     ).show()
+                    movie_recycler?.showShimmer()
+
                 }
 
                 is NetworkResult.Loading -> {
                     Log.d(TAG, "requestApiData: loading")
-                    showShimmerEffect()
+                    movie_recycler?.showShimmer()
                 }
 
             }
         })
     }
 
-
-
-    fun recyclerViewPaging() {
-
-        val upBtn = binding.upBtn
-        movie_recycler?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-                val lastVisibleItemPosition =
-                    (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
-                val itemTotalCount = recyclerView.adapter!!.itemCount - 1
-
-                Log.d(TAG, " position: $lastVisibleItemPosition")
-                Log.d(TAG, "onScrolled : Page: $page")
-
-                if (lastVisibleItemPosition > 19) {
-
-
-                    upBtn.visibility = View.VISIBLE
-                    binding.upBtn.setOnClickListener() {
-
-                        movie_recycler!!.scrollToPosition(0)
-
-                    }
-                } else binding.upBtn.visibility = View.GONE
-
-                //recyclerView 마지막일때
-                if (!movie_recycler!!.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount) {
-                    //  mSearchAdapter.deleteLoading()
-                    Log.d(TAG, "마지막 position: $lastVisibleItemPosition")
-
-
-//                    networkViewModel.getNowPlayingMovies(++page)
-
-
-                }
-
-
-            }
-        })
-    }
-    private fun showShimmerEffect() {
-
-        movie_recycler?.showShimmer()
-
-    }
 
     private fun hideShimmerEffect() {
         movie_recycler?.hideShimmer()
@@ -200,7 +196,7 @@ class SearchPostFragment : Fragment() {
     }
 
 
-    fun setRecentAdapter(movieList: List<Result>) {
+    private fun setRecentAdapter(movieList: List<Result>) {
 
         mSearchAdapter = SearchViewAdapter(movieList as MutableList<Result>, requireContext())
 
@@ -218,14 +214,13 @@ class SearchPostFragment : Fragment() {
         val searchView = menu.findItem(R.id.menu_action_search).actionView as SearchView
 
         searchView.queryHint = getString(R.string.search_view_hint)
+        Log.d(TAG, "onCreateOptionsMenu: ")
 
-
-        //recyclerVIew filter
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 Log.d(TAG, "onQueryTextSubmit: ")
                 mSearchAdapter.filter.filter(query)
-                movieList.let { mSearchAdapter?.setFilterData(it) }
+                movieList.let { mSearchAdapter.setFilterData(it) }
 
                 return false
             }
