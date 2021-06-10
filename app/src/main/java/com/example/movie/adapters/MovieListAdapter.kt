1ff38c205111b94054fd7c2707ef045dbc8a684b
KyungMin <2004ckck@naver.com>
2021-06-10 23:20:32 +0900

    [ADD] SearchViewAdapter 똑같은 코드 로직 변경

--- app/src/main/java/com/example/movie/adapters/MovieListAdapter.kt
+++ app/src/main/java/com/example/movie/adapters/MovieListAdapter.kt
@@ -3,6 +3,8 @@ package com.example.movie.adapters
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +15,7 @@ import com.example.movie.databinding.MovieListRowLayoutBinding
 import com.example.movie.models.Movie
 
 import com.example.movie.ui.fragment.MovieListFragmentDirections
+import com.example.movie.ui.fragment.SearchPostFragmentDirections
 import com.example.movie.ui.fragment.addMovie.AddDiscoverMovieFragmentDirections
 import com.example.movie.ui.fragment.addMovie.AddMovieRecentFragmentDirections
 import com.example.movie.ui.fragment.addMovie.AddPopularMovieFragmentDirections
@@ -22,15 +25,19 @@ import com.example.movie.untils.Constants.Companion.BASE_IMG_URL
 import com.example.movie.untils.Constants.Companion.TAG
 import com.example.movie.untils.MovieCase
 import com.example.movie.untils.MovieDiffUtil
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 @Suppress("CAST_NEVER_SUCCEEDS", "UNREACHABLE_CODE")
 class MovieListAdapter(
-    private var movieList: List<Result> = emptyList(),
+
     private val movieCase: MovieCase
 ) :
-    RecyclerView.Adapter<MovieListAdapter.MovieHolder>() {
+    RecyclerView.Adapter<MovieListAdapter.MovieHolder>(), Filterable {
 
+    private var listFilter: MovieListFilter? = null
+    private var movieList: MutableList<Result> = mutableListOf()
 
     class MovieHolder(
         val binding: MovieListRowLayoutBinding
@@ -67,16 +74,69 @@ class MovieListAdapter(
     }
 
 
-    override fun getItemCount(): Int {
-        return movieList.size
+    override fun getItemCount(): Int = movieList.size
+
+
+    override fun getFilter(): Filter {
+        if (listFilter == null) listFilter = MovieListFilter(this, movieList)
+
+        return listFilter as MovieListFilter
+
     }
 
+
+    // recyclerView Filter
+    class MovieListFilter(
+        private val listAdapter: MovieListAdapter,
+        private val originalData: MutableList<Result>
+    ) : Filter() {
+        private val filteredData: MutableList<Result>
+        override fun performFiltering(constraint: CharSequence): FilterResults {
+            val charString = constraint.toString()
+            filteredData.clear()
+            val results = FilterResults()
+
+            Log.d("performFiltering: ", constraint.toString())
+
+
+            if (charString.isEmpty()) {
+                filteredData.addAll(originalData)
+            } else {
+                val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
+                for (user in originalData) {
+                    if (user.title.toLowerCase(Locale.ROOT).contains(filterPattern)) {
+                        filteredData.add(user)
+                    }
+                }
+            }
+            results.values = filteredData
+            results.count = filteredData.size
+            return results
+        }
+
+        override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+
+            listAdapter.setFilterData(filterResults.values as List<Result>)
+            listAdapter.notifyDataSetChanged()
+        }
+
+        init {
+            filteredData = ArrayList()
+        }
+    }
+
+
+    fun setFilterData(movieData: List<Result>) {
+        this.movieList = movieData as MutableList<Result>
+        notifyDataSetChanged()
+
+    }
 
     fun setData(movieData: Movie) {
 
         val movieDiffUtil = MovieDiffUtil(movieList, movieData.results)
         val diffUtilResult = movieDiffUtil.let { DiffUtil.calculateDiff(it) }
-        movieList = movieData.results
+        movieList = movieData.results as MutableList<Result>
         diffUtilResult.dispatchUpdatesTo(this)
         notifyDataSetChanged()
     }
@@ -126,7 +186,7 @@ class MovieListAdapter(
                 }
             }
             MovieCase.MOVIE_POPULAR -> {
-                holder .bind(element)
+                holder.bind(element)
                 holder.binding.pagerItemPg.setOnClickListener {
                     val action =
                         AddPopularMovieFragmentDirections.actionAddPopularMovieFragmentToExampleMovieFragment(
@@ -148,9 +208,21 @@ class MovieListAdapter(
                     it.findNavController().navigate(action)
                 }
             }
+            MovieCase.MOVIE_SEARCH -> {
+                holder.bind(element)
+                holder.binding.pagerItemPg.setOnClickListener {
+                    val action =
+                        SearchPostFragmentDirections.actionSearchPostFragmentToExampleMovieFragment4(
+                            movieList[position]
+                        )
+
+                    it.findNavController().navigate(action)
+                }
 
 
+            }
         }
     }
 }
+
 
