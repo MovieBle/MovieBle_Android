2b2cfc827f347153da6e429ec02fa588a2cd2331
KyungMin <2004ckck@naver.com>
2021-06-10 19:58:42 +0900

    [CREATE] Repository 로 dataSource, RemoteSource 분리

---
+++ app/src/main/java/com/example/movie/data/Repository.kt
@@ -0,0 +1,17 @@
+package com.example.movie.data
+
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
+
+@ActivityRetainedScoped
+class Repository @Inject constructor(
+    remoteDataSource: RemoteDataSource,
+    databaseSource: DatabaseSource
+
+
+) {
+
+    val remote = remoteDataSource
+    val local = databaseSource
+
+}
