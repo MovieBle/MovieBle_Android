ea243452782a08f298de78f918dadae27a4c01a8
KyungMin <2004ckck@naver.com>
2021-06-13 19:23:37 +0900

    [CREATE] chat recycler,chat Adapter 연동

--- app/src/main/java/com/example/movie/ui/fragment/ExampleMovieFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/ExampleMovieFragment.kt
@@ -2,24 +2,46 @@ package com.example.movie.ui.fragment
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.*
 import android.widget.RatingBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.compose.LocalActivityResultRegistryOwner.current
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.movie.R
+import com.example.movie.adapters.ChatAdapter
 import com.example.movie.adapters.LikeViewAdapter
+import com.example.movie.adapters.MovieListAdapter
 import com.example.movie.data.database.entities.MovieLikeEntity
 import com.example.movie.databinding.FragmentExampleMovieBinding
+import com.example.movie.databinding.MovieChatListRowLayoutBinding
+import com.example.movie.models.ChatModel
+import com.example.movie.ui.activity.SplashActivity
+import com.example.movie.ui.activity.SplashActivity.Companion.userName
 import com.example.movie.untils.Constants.Companion.BASE_IMG_URL
 import com.example.movie.untils.Constants.Companion.TAG
+import com.example.movie.untils.MovieCase
 import com.example.movie.viewmodels.DatabaseViewModel
 import com.example.movie.viewmodels.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.todkars.shimmer.ShimmerRecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.InternalCoroutinesApi
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+import java.util.Calendar.SHORT
 
 
 @Suppress("CAST_NEVER_SUCCEEDS", "UNREACHABLE_CODE")
@@ -32,38 +54,43 @@ class ExampleMovieFragment : Fragment() {
     // 삭제 -> movieSave false
 
     // liveData로 비동기처리하면서 movieSave 상태에 따라 snackBar에 뜨는 text변경
-
+    private val db = FirebaseFirestore.getInstance()
     private var _binding: FragmentExampleMovieBinding? = null
     private val binding get() = _binding!!
 
-
+    private val currentDateTime = Calendar.getInstance().time
     private val args by navArgs<ExampleMovieFragmentArgs>()
-
+    private val auth = FirebaseAuth.getInstance()
     private val databaseViewModel: DatabaseViewModel by viewModels()
-    private val viewModel: ViewModel by viewModels()
-
+    private val viewModel: ViewModel = ViewModel()
+    private lateinit var dateFormat: String
 
     private lateinit var contents: TextView
     private lateinit var examText: TextView
     private lateinit var rating: RatingBar
     private lateinit var releaseText: TextView
-
-
+    var num = 0
     private var movieSave = false
 
+    @InternalCoroutinesApi
+    private val chatAdapter: ChatAdapter by lazy {
+        ChatAdapter(
 
-       var movieId: Int = 0
 
+        )
+    }
+    private val chatRecycler: RecyclerView by lazy {
 
-
+        binding.recyclerView
+    }
+    var movieId: Int = 0
 
     private val likeAdapter: LikeViewAdapter by lazy {
         LikeViewAdapter(
             requireContext(),
 
-        )
+            )
     }
-
 
 
     @SuppressLint("ResourceAsColor")
@@ -74,7 +101,7 @@ class ExampleMovieFragment : Fragment() {
     ): View {
 
         _binding = FragmentExampleMovieBinding.inflate(inflater, container, false)
-
+        postInfo()
 
 
 
@@ -83,21 +110,32 @@ class ExampleMovieFragment : Fragment() {
         rating = binding.movieRating
         releaseText = binding.releaseText
 
-
-        //Glide
         args.result?.poster_path?.let { viewModel.imageLoad(binding.examImg, it) }
 
         rating.rating = args.result?.vote_average!!.toFloat() / 2
-
 
         contents.text = args.result?.overview
         examText.text = args.result?.title
         releaseText.text = args.result?.release_date
 
+        binding.button3.setOnClickListener {
+
+            textNullTest()
+        }
+        setAdapter()
 
         setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    private fun setAdapter() {
+
+        chatRecycler.adapter = chatAdapter
+        chatRecycler.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        chatRecycler.setHasFixedSize(false)
+
     }
 
 
@@ -108,6 +146,13 @@ class ExampleMovieFragment : Fragment() {
         checkSaveMovie(menuItem)
     }
 
+    private fun textNullTest() {
+        if (!TextUtils.isEmpty(binding.chatEditText.text.toString().trim { it <= ' ' }))
+            postInfo()
+        else {
+            Toast.makeText(requireContext(), "내용을 입력해주세요", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
@@ -117,7 +162,6 @@ class ExampleMovieFragment : Fragment() {
 
         } else if (item.itemId == R.id.menu_action_star && movieSave) {
             deleteMovie(item)
-
         } else {
             return super.onOptionsItemSelected(item)
         }
@@ -202,14 +246,45 @@ class ExampleMovieFragment : Fragment() {
     }
 
 
-
-
-
     private fun changeMenuItemColor(item: MenuItem, color: Int) {
         item.icon.setTint(ContextCompat.getColor(requireContext(), color))
     }
 
+    //chat
+    private fun postInfo() {
 
+
+        val onlyDate: LocalDate = LocalDate.now()
+
+        dateFormat =
+            SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.KOREA).format(currentDateTime)
+        val day = onlyDate
+
+        val chatModel = ChatModel(
+
+            binding.chatEditText.text.toString(),
+            day.toString(),
+            userName.toString(),
+            num
+        )
+        Log.d(TAG, "day: $day")
+
+
+
+        db.collection("chat").document(auth.currentUser?.uid + dateFormat)
+            .set(
+                chatModel
+            )
+            .addOnSuccessListener {
+
+                Log.d(TAG, "postInfo: 성공 $chatModel")
+                binding.chatEditText.setText("")
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "postInfo: 실패  $it")
+            }
+
+    }
 
 
 }
