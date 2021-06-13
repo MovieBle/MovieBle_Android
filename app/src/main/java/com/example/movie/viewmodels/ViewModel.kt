f45667115e8dfbcf5d95ccc62ad7e68fad6252d9
KyungMin <2004ckck@naver.com>
2021-06-13 19:18:11 +0900

    [UPDATE] AndroidViewModel -> 일반 class로 변경

--- app/src/main/java/com/example/movie/viewmodels/ViewModel.kt
+++ app/src/main/java/com/example/movie/viewmodels/ViewModel.kt
@@ -1,16 +1,24 @@
 package com.example.movie.viewmodels
 
 import android.app.Application
+import android.content.Context
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import com.bumptech.glide.Glide
 import com.example.movie.R
+import com.example.movie.models.ChatModel
+import com.example.movie.ui.activity.SplashActivity.Companion.userName
 import com.example.movie.untils.App
 import com.example.movie.untils.Constants
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import kotlinx.coroutines.InternalCoroutinesApi
 
-class ViewModel(application: Application) : AndroidViewModel(application) {
+class ViewModel {
 
 
 
@@ -22,6 +30,10 @@ class ViewModel(application: Application) : AndroidViewModel(application) {
             .into(img)
     }
 
+    fun toastText(view: Context, message:String){
+        Toast.makeText(view,message,Toast.LENGTH_SHORT).show()
+    }
+
     fun showSnackBar(message: String, view: View) {
         Snackbar.make(
             view,
@@ -31,6 +43,37 @@ class ViewModel(application: Application) : AndroidViewModel(application) {
             .show()
     }
 
+
+    @InternalCoroutinesApi
+    fun startGetUserInfo() {
+        val auth = FirebaseAuth.getInstance()
+        Log.d("Mylog", "my uid : ${auth.currentUser?.uid}")
+
+        var database: DatabaseReference = FirebaseDatabase.getInstance().reference
+        database.child("users").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val userData = auth.currentUser?.uid?.let {
+                    snapshot.child(it).getValue(
+                        ChatModel::class.java
+                    )
+                }
+
+                userName = userData?.chatName
+
+
+
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+
+    }
 
 
 }
