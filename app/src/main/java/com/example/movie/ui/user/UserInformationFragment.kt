077b6d5cd3035f9e6097fb002448e862a896f59b
KyungMin <2004ckck@naver.com>
2021-06-15 20:08:39 +0900

    [CREATE] user Framgent 생성

---
+++ app/src/main/java/com/example/movie/ui/user/UserInformationFragment.kt
@@ -0,0 +1,20 @@
+package com.example.movie.ui.user
+
+import com.example.movie.R
+import com.example.movie.base.UtilityBase
+import com.example.movie.databinding.FragmentUserInformationBinding
+
+class UserInformationFragment : UtilityBase.BaseFragment<FragmentUserInformationBinding>(R.layout.fragment_user_information) {
+
+
+    override fun FragmentUserInformationBinding.onCreateView() {
+
+    }
+
+    override fun FragmentUserInformationBinding.onViewCreated() {
+    }
+
+    fun checkNameBtn(){
+
+    }
+}
---
+++ app/src/main/java/com/example/movie/ui/user/UserMovieInformationFragment.kt
@@ -0,0 +1,9 @@
+package com.example.movie.ui.user
+
+import com.example.movie.R
+import com.example.movie.base.UtilityBase
+import com.example.movie.databinding.FragmentUserMovieInformationBinding
+
+class UserMovieInformationFragment : UtilityBase.BaseFragment<FragmentUserMovieInformationBinding>(R.layout.fragment_user_movie_information) {
+
+}
