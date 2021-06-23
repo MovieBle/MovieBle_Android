7f1dc86ae599fa871ce72594a42c8565146b593c
KyungMin <2004ckck@naver.com>
2021-06-23 14:36:38 +0900

    [UPDATE] user db로 변경

--- app/src/main/java/com/example/movie/viewmodels/DatabaseViewModel.kt
+++ app/src/main/java/com/example/movie/viewmodels/DatabaseViewModel.kt
@@ -9,11 +9,13 @@ import androidx.lifecycle.*
 import com.example.movie.data.Repository
 import com.example.movie.models.Movie
 import com.example.movie.data.database.entities.MovieLikeEntity
+import com.example.movie.data.database.entities.UserEntity
 import com.example.movie.data.database.entities.movie.*
 import com.example.movie.untils.NetworkResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import retrofit2.Response
 import javax.inject.Inject
@@ -22,7 +24,7 @@ import javax.inject.Inject
 @HiltViewModel
 class DatabaseViewModel @Inject constructor(
     application: Application,
-     val repository: Repository
+    private val repository: Repository
 
 ) :
     AndroidViewModel(application) {
@@ -30,19 +32,8 @@ class DatabaseViewModel @Inject constructor(
     // 데이터가 있지 여부
     val emptyDatabase: MutableLiveData<Boolean> = MutableLiveData(true)
 
-    val getAllLikeData: LiveData<List<MovieLikeEntity>> =
-        repository.local.getLikeAllMovie().asLiveData()
+
     val getAllData: LiveData<List<MovieEntity>> = repository.local.getAllMovie().asLiveData()
-    val getAllPopularData: LiveData<List<MoviePopularEntity>> =
-        repository.local.getAllPopularMovie().asLiveData()
-    val getAllDiscoverData: LiveData<List<MovieDiscoverEntity>> =
-        repository.local.getAllDiscoverMovie().asLiveData()
-    val getAllRecentData: LiveData<List<MovieRecentEntity>> =
-        repository.local.getAllRecentMovie().asLiveData()
-
-
-    val getAllMovieListData: LiveData<List<MovieListEntity>> =
-        repository.local.getAllMovieListMovie().asLiveData()
 
     val getAllMovie: MutableLiveData<NetworkResult<Movie>> = MutableLiveData()
 
@@ -58,73 +49,12 @@ class DatabaseViewModel @Inject constructor(
         }
     }
 
-    private fun insertPopularMovie(movie: MoviePopularEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.local.insertPopularMovie(movie)
-        }
-    }
-
-    private fun insertRecentMovie(movie: MovieRecentEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.local.insertRecentMovie(movie)
-        }
-    }
-
-    private fun insertDiscoverMovie(movie: MovieDiscoverEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.local.insertDiscoverMovie(movie)
-        }
-    }
-
-
-    private fun insertMovieListMovie(movie: MovieListEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.local.insertMovieListMovie(movie)
-        }
-    }
 
     @InternalCoroutinesApi
-    fun deleteLikeMovie(movie: MovieLikeEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.local.deleteLikeMovie(movie)
-        }
-    }
-
-    fun insertLikeMovie(movie: MovieLikeEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.local.insertLikeMovie(movie)
-        }
-    }
-
-    @InternalCoroutinesApi
-    fun getDiscoverMovie(queries: Map<String, String>, page: Int) = viewModelScope.launch {
+    fun getAllMovie(page: Int) = viewModelScope.launch {
 
 
-        getDiscoverMoviesSafeCall(queries, page)
-
-    }
-
-    @InternalCoroutinesApi
-    fun getRecentMovie(queries: Map<String, String>, page: Int) = viewModelScope.launch {
-        getRecentMovieSafeCall(queries, page)
-
-    }
-
-    @InternalCoroutinesApi
-    fun getTopMovie(queries: Map<String, String>, page: Int) = viewModelScope.launch {
-        getTopMovieSafeCall(queries, page)
-
-    }
-
-    @InternalCoroutinesApi
-    fun getPopularMovie(queries: Map<String, String>, page: Int) = viewModelScope.launch {
-        getPopularMovieSafeCall(queries, page)
-
-    }
-
-    @InternalCoroutinesApi
-     fun getMovieList(queries: Map<String, String>, page: Int) = viewModelScope.launch {
-        getMovieListSafeCall(queries, page)
+        getDiscoverMoviesSafeCall(page)
 
     }
 
@@ -135,192 +65,31 @@ class DatabaseViewModel @Inject constructor(
         insertMovie(movieEntity)
     }
 
-    @InternalCoroutinesApi
-    private fun offlineCashPopularMovie(movie: Movie) {
-
-        val movieEntity = MoviePopularEntity(movie)
-        insertPopularMovie(movieEntity)
-    }
 
     @InternalCoroutinesApi
-    private fun offlineCashRecentMovie(movie: Movie) {
-
-        val movieEntity = MovieRecentEntity(movie)
-        insertRecentMovie(movieEntity)
-    }
-
-    @InternalCoroutinesApi
-    private fun offlineCashDiscoverMovie(movie: Movie) {
-
-        val movieEntity = MovieDiscoverEntity(movie)
-        insertDiscoverMovie(movieEntity)
-    }
-
-    @InternalCoroutinesApi
-    private fun offlineCashMovieListMovie(movie: Movie) {
-
-        val movieEntity = MovieListEntity(movie)
-        insertMovieListMovie(movieEntity)
-    }
-
-
-    @InternalCoroutinesApi
-    private suspend fun getDiscoverMoviesSafeCall(queries: Map<String, String>, page: Int) {
+    private suspend fun getDiscoverMoviesSafeCall(page: Int) {
 
         getAllMovie.value = NetworkResult.Loading()
         //인터넷이 연결되었을 때
         if (hasInternetConnection()) {
 
 
-            try {
+            val response = repository.remote.getMovie(page)
+            getAllMovie.value = handleMovieResponse(response)
 
 
-                val response = repository.remote.getDiscoverMovie(queries, page)
-                getAllMovie.value = handleMovieResponse(response)
+            val movieData = getAllMovie.value!!.data
+            Log.d(TAG, "getDiscoverMoviesSafeCall: $movieData")
 
+            if (movieData != null) {
 
-                val movieData = getAllMovie.value!!.data
-                Log.d(TAG, "getDiscoverMoviesSafeCall: $movieData")
-
-                if (movieData != null) {
-
-                    offlineCashDiscoverMovie(movieData)
-                }
-
-
-            } catch (e: Exception) {
-
-                getAllMovie.value = NetworkResult.Error("영화를 찾을수 없음")
+                offlineCashMovie(movieData)
             }
-        } else {
-            // 인터넷 연결 실패
-            getAllMovie.value = NetworkResult.Error("No Internet Connection.")
-        }
 
-    }
-
-
-    private suspend fun getRecentMovieSafeCall(queries: Map<String, String>, page: Int) {
-
-
-        getAllMovie.value = NetworkResult.Loading()
-
-        //인터넷이 연결되었을 때
-        if (hasInternetConnection()) {
-            try {
-                val response = repository.remote.getRecentMovie(queries, page)
-
-                getAllMovie.value = handleMovieResponse1(response)
-
-                val movieData = getAllMovie.value!!.data
-                Log.d(TAG, "getRecentMovieSafeCall: $movieData}")
-                if (movieData != null) {
-
-                    offlineCashRecentMovie(movieData)
-                }
-            } catch (e: Exception) {
-                getAllMovie.value = NetworkResult.Error("영화를 찾을수 없음")
-
-            }
-        } else {
-            // 인터넷 연결 실패
-            getAllMovie.value = NetworkResult.Error("No Internet Connection.")
-
-
-        }
-
-    }
-
-
-    private suspend fun getPopularMovieSafeCall(queries: Map<String, String>, page: Int) {
-
-        getAllMovie.value = NetworkResult.Loading()
-        //인터넷이 연결되었을 때
-        if (hasInternetConnection()) {
-
-            try {
-                val response = repository.remote.getPopularMovie(queries, page)
-                getAllMovie.value = handleMovieResponse2(response)
-                val movieData = getAllMovie.value!!.data
-
-                Log.d(TAG, "getPopularMovieSafeCall: $movieData")
-                if (movieData != null) {
-
-                    offlineCashPopularMovie(movieData)
-                }
-
-
-            } catch (e: Exception) {
-                getAllMovie.value = NetworkResult.Error("영화를 찾을수 없음")
-
-            }
-        } else {
-            // 인터넷 연결 실패
-            getAllMovie.value = NetworkResult.Error("No Internet Connection.")
 
         }
     }
 
-
-    private suspend fun getTopMovieSafeCall(queries: Map<String, String>, page: Int) {
-
-        getAllMovie.value = NetworkResult.Loading()
-
-        //인터넷이 연결되었을 때
-        if (hasInternetConnection()) {
-
-            try {
-                val response = repository.remote.getTopMovie(queries, page)
-                getAllMovie.value = handleMovieResponse3(response)
-                val movieData = getAllMovie.value!!.data
-                if (movieData != null) {
-
-                    offlineCashMovie(movieData)
-                }
-
-            } catch (e: Exception) {
-                getAllMovie.value = NetworkResult.Error("영화를 찾을수 없음")
-
-            }
-        } else {
-            // 인터넷 연결 실패
-            getAllMovie.value = NetworkResult.Error("No Internet Connection.")
-
-
-        }
-
-    }
-
-
-    private suspend fun getMovieListSafeCall(queries: Map<String, String>, page: Int) {
-
-        getAllMovie.value = NetworkResult.Loading()
-
-        //인터넷이 연결되었을 때
-        if (hasInternetConnection()) {
-
-            try {
-                val response = repository.remote.getMovieList(queries, page)
-                getAllMovie.value = handleMovieResponse3(response)
-
-                val movieData = getAllMovie.value!!.data
-                if (movieData != null) {
-
-                    offlineCashMovieListMovie(movieData)
-                }
-
-            } catch (e: Exception) {
-                getAllMovie.value = NetworkResult.Error("영화를 찾을수 없음")
-
-            }
-        } else {
-            // 인터넷 연결 실패
-            getAllMovie.value = NetworkResult.Error("No Internet Connection.")
-
-
-        }
-
-    }
 
     private fun handleMovieResponse(response: Response<Movie>): NetworkResult<Movie> {
 
@@ -338,8 +107,7 @@ class DatabaseViewModel @Inject constructor(
             }
 
             //데이터가 없을 때
-            response.body()!!.results.isNullOrEmpty() -> {
-
+            response.body()!!.isNullOrEmpty() -> {
                 return NetworkResult.Error("API 연동 실패")
             }
 
@@ -347,7 +115,6 @@ class DatabaseViewModel @Inject constructor(
             response.isSuccessful -> {
 
                 val movieData = response.body()
-                Log.d(TAG, "handleMovieResponse Discover: $movieData")
                 return NetworkResult.Success(movieData!!)
             }
             else -> {
@@ -358,113 +125,6 @@ class DatabaseViewModel @Inject constructor(
         }
     }
 
-    private fun handleMovieResponse1(response: Response<Movie>): NetworkResult<Movie> {
-
-        when {
-            //너무 오래 걸릴 때
-            response.message().toString().contains("Timeout") -> {
-
-                return NetworkResult.Error("Timeout")
-            }
-
-            // 402가 뜰 때
-            response.code() == 402 -> {
-
-                return NetworkResult.Error("API Key Limited.")
-            }
-
-            //데이터가 없을 때
-            response.body()!!.results.isNullOrEmpty() -> {
-
-                return NetworkResult.Error("API 연동 실패")
-            }
-
-            //성공했을 때 data 를 가져옴
-            response.isSuccessful -> {
-
-                val movieData = response.body()
-                Log.d(TAG, "handleMovieResponse Recent: $movieData")
-                return NetworkResult.Success(movieData!!)
-            }
-            else -> {
-
-                return NetworkResult.Error(response.message())
-            }
-
-        }
-    }
-
-    private fun handleMovieResponse2(response: Response<Movie>): NetworkResult<Movie> {
-
-        when {
-            //너무 오래 걸릴 때
-            response.toString().contains("Timeout") -> {
-
-                return NetworkResult.Error("Timeout")
-            }
-
-            // 402가 뜰 때
-            response.code() == 402 -> {
-
-                return NetworkResult.Error("API Key Limited.")
-            }
-
-            //데이터가 없을 때
-            response.body()?.results.isNullOrEmpty() -> {
-
-                return NetworkResult.Error("API 연동 실패")
-            }
-
-            //성공했을 때 data 를 가져옴
-            response.isSuccessful -> {
-
-                val movieData = response.body()
-                Log.d(TAG, "handleMovieResponse Popular: $movieData")
-                return NetworkResult.Success(movieData!!)
-            }
-            else -> {
-
-                return NetworkResult.Error(response?.message())
-            }
-
-        }
-    }
-
-    private fun handleMovieResponse3(response: Response<Movie>): NetworkResult<Movie> {
-
-        when {
-            //너무 오래 걸릴 때
-            response.message().toString().contains("Timeout") -> {
-
-                return NetworkResult.Error("Timeout")
-            }
-
-            // 402가 뜰 때
-            response.code() == 402 -> {
-
-                return NetworkResult.Error("API Key Limited.")
-            }
-
-            //데이터가 없을 때
-            response.body()!!.results.isNullOrEmpty() -> {
-
-                return NetworkResult.Error("API 연동 실패")
-            }
-
-            //성공했을 때 data 를 가져옴
-            response.isSuccessful -> {
-
-                val movieData = response.body()
-                Log.d(TAG, "handleMovieResponse Top: $movieData")
-                return NetworkResult.Success(movieData!!)
-            }
-            else -> {
-
-                return NetworkResult.Error(response.message())
-            }
-
-        }
-    }
 
     private fun hasInternetConnection(): Boolean {
         val connectivityManager = getApplication<Application>().getSystemService(
@@ -481,9 +141,7 @@ class DatabaseViewModel @Inject constructor(
         }
     }
 
-    fun checkIfDatabaseEmpty(movieData: List<MovieLikeEntity>) {
-        emptyDatabase.value = movieData.isEmpty() //데이터 있음
-    }
+
 
 
 }
