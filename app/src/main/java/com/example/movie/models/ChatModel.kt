9ce7ea4e595d2fd68f011440b3a5d84a2d514185
KyungMin <2004ckck@naver.com>
2021-06-23 14:19:56 +0900

    [DELETE] chat

--- app/src/main/java/com/example/movie/models/ChatModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movie.models
-
-import android.widget.ImageView
-
-data class ChatModel(val chatText:String,val chatDay:String,val chatName:String,val chatLikeText:Int){
-    constructor() : this("", "", "", 0)
-}
