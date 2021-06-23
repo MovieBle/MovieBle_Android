0ae414a0e30ffab0548f305fae2cd0860fae9d46
KyungMin <2004ckck@naver.com>
2021-06-23 14:43:00 +0900

    [DELETE] api 바꾸면서 class 제거

--- app/src/main/java/com/example/movie/models/kinds/Genre.kt
+++
@@ -1,6 +0,0 @@
-package com.example.movie.models.kinds
-
-data class Genre(
-    val id: Int,
-    val name: String
-)
--- app/src/main/java/com/example/movie/models/kinds/MovieKinds.kt
+++
@@ -1,5 +0,0 @@
-package com.example.movie.models.kinds
-
-data class MovieKinds(
-    val genres: List<Genre>
-)
