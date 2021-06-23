c22577d5d063c2cefd8d05135036a0d17303f090
KyungMin <2004ckck@naver.com>
2021-06-23 14:25:17 +0900

    [UPDATE] 안쓸 코드 주석 처리

--- app/src/main/java/com/example/movie/ui/fragment/ExampleMovieFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/ExampleMovieFragment.kt
@@ -2,51 +2,24 @@ package com.example.movie.ui.fragment
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
 import android.view.*
 import android.widget.RatingBar
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.compose.LocalActivityResultRegistryOwner.current
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.movie.R
-import com.example.movie.adapters.ChatAdapter
-import com.example.movie.adapters.LikeViewAdapter
-import com.example.movie.adapters.MovieListAdapter
-import com.example.movie.data.database.entities.MovieLikeEntity
 import com.example.movie.databinding.FragmentExampleMovieBinding
-import com.example.movie.databinding.MovieChatListRowLayoutBinding
-import com.example.movie.models.ChatModel
-import com.example.movie.ui.activity.SplashActivity
-import com.example.movie.ui.activity.SplashActivity.Companion.userName
-import com.example.movie.untils.Constants.Companion.BASE_IMG_URL
-import com.example.movie.untils.Constants.Companion.TAG
-import com.example.movie.untils.MovieCase
 import com.example.movie.viewmodels.DatabaseViewModel
-import com.example.movie.viewmodels.ViewModel
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.todkars.shimmer.ShimmerRecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.InternalCoroutinesApi
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
-import java.util.Calendar.SHORT
 
 
 @Suppress("CAST_NEVER_SUCCEEDS", "UNREACHABLE_CODE")
 @InternalCoroutinesApi
 @AndroidEntryPoint
+
 class ExampleMovieFragment : Fragment() {
 
 
@@ -54,43 +27,23 @@ class ExampleMovieFragment : Fragment() {
     // 삭제 -> movieSave false
 
     // liveData로 비동기처리하면서 movieSave 상태에 따라 snackBar에 뜨는 text변경
-    private val db = FirebaseFirestore.getInstance()
     private var _binding: FragmentExampleMovieBinding? = null
-    private val binding get() = _binding!!
+     val binding get() = _binding!!
 
-    private val currentDateTime = Calendar.getInstance().time
-    private val args by navArgs<ExampleMovieFragmentArgs>()
-    private val auth = FirebaseAuth.getInstance()
+    //private val args by navArgs<ExampleMovieFragmentArgs>()
     private val databaseViewModel: DatabaseViewModel by viewModels()
-    private val viewModel: ViewModel = ViewModel()
-    private lateinit var dateFormat: String
-
     private lateinit var contents: TextView
     private lateinit var examText: TextView
     private lateinit var rating: RatingBar
     private lateinit var releaseText: TextView
-    var num = 0
     private var movieSave = false
 
-    @InternalCoroutinesApi
-    private val chatAdapter: ChatAdapter by lazy {
-        ChatAdapter(
 
-
-        )
-    }
     private val chatRecycler: RecyclerView by lazy {
 
         binding.recyclerView
     }
     var movieId: Int = 0
-
-    private val likeAdapter: LikeViewAdapter by lazy {
-        LikeViewAdapter(
-            requireContext(),
-
-            )
-    }
 
 
     @SuppressLint("ResourceAsColor")
@@ -101,7 +54,6 @@ class ExampleMovieFragment : Fragment() {
     ): View {
 
         _binding = FragmentExampleMovieBinding.inflate(inflater, container, false)
-        postInfo()
 
 
 
@@ -110,50 +62,31 @@ class ExampleMovieFragment : Fragment() {
         rating = binding.movieRating
         releaseText = binding.releaseText
 
-        args.result?.poster_path?.let { viewModel.imageLoad(binding.examImg, it) }
+//        args.result?.poster_path?.let { viewModel.imageLoad(binding.examImg, it) }
 
-        rating.rating = args.result?.vote_average!!.toFloat() / 2
-
-        contents.text = args.result?.overview
-        examText.text = args.result?.title
-        releaseText.text = args.result?.release_date
-
-        binding.button3.setOnClickListener {
-
-            textNullTest()
-        }
-        setAdapter()
+//        rating.rating = args.result?.vote_average!!.toFloat() / 2
+//
+//        contents.text = args.result?.overview
+//        examText.text = args.result?.title
+//        releaseText.text = args.result?.release_date
 
         setHasOptionsMenu(true)
 
         return binding.root
     }
 
-    private fun setAdapter() {
-
-        chatRecycler.adapter = chatAdapter
-        chatRecycler.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        chatRecycler.setHasFixedSize(false)
-
-    }
 
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_exam, menu)
-        val menuItem = menu.findItem(R.id.menu_action_star)
-        changeMenuItemColor(menuItem, R.color.white)
-        checkSaveMovie(menuItem)
-    }
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.menu_exam, menu)
+//        val menuItem = menu.findItem(R.id.menu_action_star)
+//        changeMenuItemColor(menuItem, R.color.white)
+//        checkSaveMovie(menuItem)
+//    }
 
-    private fun textNullTest() {
-        if (!TextUtils.isEmpty(binding.chatEditText.text.toString().trim { it <= ' ' }))
-            postInfo()
-        else {
-            Toast.makeText(requireContext(), "내용을 입력해주세요", Toast.LENGTH_SHORT).show()
-        }
-    }
 
+
+    /*
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         if (item.itemId == R.id.menu_action_star && !movieSave) {
@@ -168,28 +101,31 @@ class ExampleMovieFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
+     */
 
-    private fun checkSaveMovie(menuItem: MenuItem) {
-
-        // ROOM 과 jSON 객체로 받아온 id 값을 비교
-        databaseViewModel.getAllLikeData.observe(this, { favoritesEntity ->
-            try {
-                for (savedMovie in favoritesEntity) {
-                    if (savedMovie.result.id == args.result?.id) {
-                        changeMenuItemColor(menuItem, R.color.yellow)
-                        movieId = savedMovie.id
-                        movieSave = true
-                    } else {
-                        changeMenuItemColor(menuItem, R.color.white)
-                    }
-                }
-            } catch (e: Exception) {
-                Log.d("DetailsActivity", e.message.toString())
-            }
-        })
-    }
+/*
+//    private fun checkSaveMovie(menuItem: MenuItem) {
+//
+//        // ROOM 과 jSON 객체로 받아온 id 값을 비교
+//        databaseViewModel.getAllLikeData.observe(this, { favoritesEntity ->
+//            try {
+//                for (savedMovie in favoritesEntity) {
+//                    if (savedMovie.result.id == args.result?.id) {
+//                        changeMenuItemColor(menuItem, R.color.yellow)
+//                        movieId = savedMovie.id
+//                        movieSave = true
+//                    } else {
+//                        changeMenuItemColor(menuItem, R.color.white)
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                Log.d("DetailsActivity", e.message.toString())
+//            }
+//        })
+//    }
 
     //데이터 저장
+
     private fun insertMovie(item: MenuItem) {
         val movieData = args.result.let {
             MovieLikeEntity(
@@ -198,7 +134,7 @@ class ExampleMovieFragment : Fragment() {
             )
         }
 
-        databaseViewModel.insertLikeMovie(movieData)
+//        databaseViewModel.insertLikeMovie(movieData)
 
         viewModel.showSnackBar("포스트가 저장되었습니다.", requireView())
 
@@ -213,8 +149,10 @@ class ExampleMovieFragment : Fragment() {
             TAG,
             "insertMovie: ${BASE_IMG_URL + args.result?.poster_path}, ${args.result?.title}"
         )
-    }
+    }'
 
+ */
+/*
     private fun deleteMovie(item: MenuItem) {
 
 
@@ -225,66 +163,35 @@ class ExampleMovieFragment : Fragment() {
             )
         }
 
-
-        movieData?.let {
-            databaseViewModel.deleteLikeMovie(it)
-            likeAdapter.setLikeData(listOf(movieData))
-            viewModel.showSnackBar("포스트가 지워졌습니다.", requireView())
+ */
 
 
-            movieSave = false
+//        movieData?.let {
+//            databaseViewModel.deleteLikeMovie(it)
+//            likeAdapter.setLikeData(listOf(movieData))
+//            viewModel.showSnackBar("포스트가 지워졌습니다.", requireView())
+//
+//
+//            movieSave = false
+//
+//            changeMenuItemColor(item, R.color.white)
+//
+//        }
 
-            changeMenuItemColor(item, R.color.white)
-
-        }
 
 
+      //  Log.d(TAG, "deleteMovie: $movieData")
 
-        Log.d(TAG, "deleteMovie: $movieData")
 
+    //}
 
-    }
 
 
     private fun changeMenuItemColor(item: MenuItem, color: Int) {
         item.icon.setTint(ContextCompat.getColor(requireContext(), color))
     }
 
-    //chat
-    private fun postInfo() {
 
-
-        val onlyDate: LocalDate = LocalDate.now()
-
-        dateFormat =
-            SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.KOREA).format(currentDateTime)
-        val day = onlyDate
-
-        val chatModel = ChatModel(
-
-            binding.chatEditText.text.toString(),
-            day.toString(),
-            userName.toString(),
-            num
-        )
-        Log.d(TAG, "day: $day")
-
-
-
-        db.collection("chat").document(auth.currentUser?.uid + dateFormat)
-            .set(
-                chatModel
-            )
-            .addOnSuccessListener {
-
-                Log.d(TAG, "postInfo: 성공 $chatModel")
-                binding.chatEditText.setText("")
-            }
-            .addOnFailureListener {
-                Log.d(TAG, "postInfo: 실패  $it")
-            }
-
-    }
 
 
 }
--- app/src/main/java/com/example/movie/ui/fragment/ExampleMovieLikeFragment.kt
+++ app/src/main/java/com/example/movie/ui/fragment/ExampleMovieLikeFragment.kt
@@ -1,35 +1,22 @@
 package com.example.movie.ui.fragment
 
 import android.os.Bundle
-import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.RatingBar
 import android.widget.TextView
-import androidx.activity.addCallback
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import androidx.navigation.ui.navigateUp
-import com.example.movie.R
-import com.example.movie.adapters.LikeViewAdapter
-import com.example.movie.data.database.entities.MovieLikeEntity
 import com.example.movie.databinding.FragmentExampleLikeMovieBinding
-import com.example.movie.untils.Constants
 import com.example.movie.viewmodels.DatabaseViewModel
-import com.example.movie.viewmodels.QueryViewModel
-import com.example.movie.viewmodels.ViewModel
 import kotlinx.coroutines.InternalCoroutinesApi
-import javax.xml.parsers.DocumentBuilderFactory.newInstance
 
+@Suppress("UNCHECKED_CAST")
 class ExampleMovieLikeFragment : Fragment() {
 
     private var _binding: FragmentExampleLikeMovieBinding? = null
-    private val binding get() = _binding!!
-    private val args by navArgs<ExampleMovieLikeFragmentArgs>()
+     val binding get() = _binding!!
 
 
     private lateinit var contents: TextView
@@ -44,15 +31,7 @@ class ExampleMovieLikeFragment : Fragment() {
 
     @InternalCoroutinesApi
     private lateinit var databaseViewModel: DatabaseViewModel
-    private val viewModel: ViewModel by viewModels()
 
-    @InternalCoroutinesApi
-    private val likeAdapter: LikeViewAdapter by lazy {
-        LikeViewAdapter(
-            requireContext(),
-
-            )
-    }
     @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,13 +59,6 @@ class ExampleMovieLikeFragment : Fragment() {
 
 
 
-        rating.rating = args.likeMovie.result.vote_average.toFloat() / 2
-        contents.text = args.likeMovie.result.overview
-        examText.text = args.likeMovie.result.title
-        releaseText.text = args.likeMovie.result.release_date
-
-        viewModel.imageLoad(binding.likeExamImg, args.likeMovie.result.poster_path)
-
 
 
         setHasOptionsMenu(true)
@@ -94,78 +66,80 @@ class ExampleMovieLikeFragment : Fragment() {
         return binding.root
     }
 
-    @InternalCoroutinesApi
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_exam, menu)
-        val menuItem = menu.findItem(R.id.menu_action_star)
-        changeMenuItemColor(menuItem, R.color.yellow)
-        checkSaveMovie(menuItem)
-    }
+//    @InternalCoroutinesApi
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.menu_exam, menu)
+//        val menuItem = menu.findItem(R.id.menu_action_star)
+//        changeMenuItemColor(menuItem, R.color.yellow)
+//        checkSaveMovie(menuItem)
+//    }
+//
+//    @InternalCoroutinesApi
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//
+//        if (item.itemId == R.id.menu_action_star && movieSave) {
+//            deleteMovie(item)
+//        } else {
+//            return super.onOptionsItemSelected(item)
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
 
-    @InternalCoroutinesApi
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//    @InternalCoroutinesApi
+//    private fun deleteMovie(item: MenuItem) {
+//
+//
+//        val movieData = args.likeMovie?.result.let {
+//            args.likeMovie?.result?.let { it1 ->
+//                MovieLikeEntity(
+//                    movieId--,
+//                    it1
+//                )
+//            }
+//        }
+//
 
-        if (item.itemId == R.id.menu_action_star && movieSave) {
-            deleteMovie(item)
-        } else {
-            return super.onOptionsItemSelected(item)
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    @InternalCoroutinesApi
-    private fun deleteMovie(item: MenuItem) {
-
-
-        val movieData = args.likeMovie.result.let {
-            MovieLikeEntity(
-                movieId--,
-                args.likeMovie.result
-            )
-        }
-
-
-        movieData.let {
-            databaseViewModel.deleteLikeMovie(movieData)
-            viewModel.showSnackBar("포스트가 지워졌습니다.", requireView())
-
-            likeAdapter.setLikeData(listOf(movieData))
-            movieSave = false
-
-            changeMenuItemColor(item, R.color.white)
-        }
-
-        findNavController().navigate(R.id.action_exampleLikeMovieFragment_to_movieListFragment)
+//        movieData.let {
+//            movieData?.let { it1 -> databaseViewModel.deleteLikeMovie(it1) }
+//            viewModel.showSnackBar("포스트가 지워졌습니다.", requireView())
+//
+//            likeAdapter.setLikeData(listOf(movieData) as List<MovieLikeEntity>)
+//            movieSave = false
+//
+//            changeMenuItemColor(item, R.color.white)
+//        }
+//
+//        findNavController().navigate(R.id.action_exampleLikeMovieFragment_to_movieListFragment)
+//
+//
+//        Log.d(Constants.TAG, "deleteMovie: $movieData")
+//    }
 
 
-        Log.d(Constants.TAG, "deleteMovie: $movieData")
-    }
-
-
-    @InternalCoroutinesApi
-    private fun checkSaveMovie(menuItem: MenuItem) {
-
-        // ROOM 과 jSON 객체로 받아온 id 값을 비교
-        databaseViewModel.getAllLikeData.observe(this, { favoritesEntity ->
-            try {
-                for (savedMovie in favoritesEntity) {
-                    if (savedMovie.result.id == args.likeMovie.result?.id) {
-                        changeMenuItemColor(menuItem, R.color.yellow)
-                        movieId = savedMovie.id
-                        movieSave = true
-                    } else {
-                        changeMenuItemColor(menuItem, R.color.white)
-                    }
-                }
-            } catch (e: Exception) {
-                Log.d("DetailsActivity", e.message.toString())
-            }
-        })
-    }
-
-    private fun changeMenuItemColor(item: MenuItem, color: Int) {
-        item.icon.setTint(ContextCompat.getColor(requireContext(), color))
-    }
+//    @InternalCoroutinesApi
+//    private fun checkSaveMovie(menuItem: MenuItem) {
+//
+//        // ROOM 과 jSON 객체로 받아온 id 값을 비교
+//        databaseViewModel.getAllLikeData.observe(this, { favoritesEntity ->
+//            try {
+//                for (savedMovie in favoritesEntity) {
+//                    if (savedMovie.result.id == args.likeMovie?.result?.id) {
+//                        changeMenuItemColor(menuItem, R.color.yellow)
+//                        movieId = savedMovie.id
+//                        movieSave = true
+//                    } else {
+//                        changeMenuItemColor(menuItem, R.color.white)
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                Log.d("DetailsActivity", e.message.toString())
+//            }
+//        })
+//    }
+//
+//    private fun changeMenuItemColor(item: MenuItem, color: Int) {
+//        item.icon.setTint(ContextCompat.getColor(requireContext(), color))
+//    }
 
 
 
