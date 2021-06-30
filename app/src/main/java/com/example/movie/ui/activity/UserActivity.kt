fd5a354408d04821038b0960ce515e1737a78386
KyungMin <2004ckck@naver.com>
2021-06-30 18:14:11 +0900

    [UPDATE] 화면 fullscreen 으로 변경

--- app/src/main/java/com/example/movie/ui/activity/UserActivity.kt
+++ app/src/main/java/com/example/movie/ui/activity/UserActivity.kt
@@ -1,12 +1,18 @@
 package com.example.movie.ui.activity
 
+import android.view.WindowManager
 import com.example.movie.R
 import com.example.movie.base.UtilityBase
 import com.example.movie.databinding.ActivityUserBinding
 
+@Suppress("DEPRECATION")
 class UserActivity : UtilityBase.BaseActivity<ActivityUserBinding>(R.layout.activity_user) {
     override fun ActivityUserBinding.onCreate() {
         supportActionBar!!.title = ""
         supportActionBar!!.hide()
+         window.setFlags(
+             WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
