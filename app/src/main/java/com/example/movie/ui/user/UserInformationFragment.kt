0f9acf7f5d1e7a65d86ab448499f1531de3a243a
KyungMin <2004ckck@naver.com>
2021-06-30 18:06:38 +0900

    [ADD] user class 생성

--- app/src/main/java/com/example/movie/ui/user/UserInformationFragment.kt
+++ app/src/main/java/com/example/movie/ui/user/UserInformationFragment.kt
@@ -1,20 +1,24 @@
 package com.example.movie.ui.user
 
+import androidx.navigation.fragment.findNavController
 import com.example.movie.R
 import com.example.movie.base.UtilityBase
 import com.example.movie.databinding.FragmentUserInformationBinding
 
-class UserInformationFragment : UtilityBase.BaseFragment<FragmentUserInformationBinding>(R.layout.fragment_user_information) {
+class UserInformationFragment :
+    UtilityBase.BaseFragment<FragmentUserInformationBinding>(R.layout.fragment_user_information) {
 
 
     override fun FragmentUserInformationBinding.onCreateView() {
+        binding.activity = this@UserInformationFragment
 
     }
 
     override fun FragmentUserInformationBinding.onViewCreated() {
     }
 
-    fun checkNameBtn(){
+    fun checkNameBtn() {
 
+        findNavController().navigate(R.id.action_userInformationFragment_to_UserMovieInformationFragment)
     }
 }
--- app/src/main/java/com/example/movie/ui/user/UserMovieInformationFragment.kt
+++ app/src/main/java/com/example/movie/ui/user/UserMovieInformationFragment.kt
@@ -1,9 +1,23 @@
 package com.example.movie.ui.user
 
+import androidx.fragment.app.viewModels
 import com.example.movie.R
 import com.example.movie.base.UtilityBase
 import com.example.movie.databinding.FragmentUserMovieInformationBinding
+import com.example.movie.viewmodels.UserViewModel
 
-class UserMovieInformationFragment : UtilityBase.BaseFragment<FragmentUserMovieInformationBinding>(R.layout.fragment_user_movie_information) {
+class UserMovieInformationFragment :
+    UtilityBase.BaseFragment<FragmentUserMovieInformationBinding>(R.layout.fragment_user_movie_information) {
+    private val userViewModel: UserViewModel by viewModels()
 
+    override fun FragmentUserMovieInformationBinding.onCreateView() {
+        binding.fragment = this@UserMovieInformationFragment
+        binding.viewModel = userViewModel
+        binding.lifecycleOwner = this@UserMovieInformationFragment
+    }
+
+
+    override fun FragmentUserMovieInformationBinding.onViewCreated() {
+
+    }
 }
