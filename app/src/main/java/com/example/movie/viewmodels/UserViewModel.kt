32c1e9605b4387f06ffa66b6df9ec8093f83be35
KyungMin <2004ckck@naver.com>
2021-06-30 18:06:20 +0900

    [ADD] 주석 추가

--- app/src/main/java/com/example/movie/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/example/movie/viewmodels/UserViewModel.kt
@@ -4,17 +4,23 @@ import android.app.Application
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.Navigation.findNavController
 import com.example.movie.R
 import com.example.movie.untils.Constants
 
 class UserViewModel(application: Application) : AndroidViewModel(application) {
-     val isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    // 뮤터블은 값을 수정, 삭제, 추가 가능
+    private val _isLoading = MutableLiveData(false)
+
+    // private 을 get() 으로 초기화 한 이유
+    // 객체지향의 캡슐화를 지키기 위해서다.
+    val isLoading: MutableLiveData<Boolean> get() = _isLoading
 
 
     fun checkBtn() {
-        Log.d(Constants.TAG, "checkBtn: ")
         isLoading.value = true
     }
 
