5db122b77f5d69824d42f40f14941915267994e2
KyungMin <2004ckck@naver.com>
2021-07-01 16:35:05 +0900

    [UPDATE] base url 변경

--- app/src/main/java/com/example/movie/di/NetworkModule.kt
+++ app/src/main/java/com/example/movie/di/NetworkModule.kt
@@ -2,10 +2,8 @@ package com.example.movie.di
 
 import android.content.ContentValues
 import android.util.Log
-import com.example.movie.network.ApiInterface
-import com.example.movie.untils.Constants
+import com.example.movie.data.network.ApiInterface
 import com.example.movie.untils.Constants.Companion.BASE_URL
-import com.example.movie.untils.Constants.Companion.TAG
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,6 +18,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 
+
 // Singleton, Provides 쓸때 private 말고 public 으로 해야한다.
 object NetworkModule {
 
@@ -29,10 +28,14 @@ object NetworkModule {
     fun provideHttpClient(): OkHttpClient {
         Log.d(ContentValues.TAG, "provideHttpClient: ")
         return OkHttpClient.Builder()
-            //제한시간 15초
+            //서버로부터의 응답까지의 시간이 읽기 시간 초과보다 크면 요청 실패로 판단한다.
             .readTimeout(15, TimeUnit.SECONDS)
-            //연결시간 15초
+            //서버로 요청을 시작한 후 15초가 지날 때 까지 데이터가 안오면 에러로 판단한다.
             .connectTimeout(15, TimeUnit.SECONDS)
+            // 얼마나 빨리 서버로 데이터를 받을 수 있는지 판단한다.
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .// 이 클라이언트를 통해 오고 가는 네트워크 요청/응답을 로그로 표시하도록 합니다.
+            addInterceptor(getLoggingInterceptor())
             .build()
 
     }
@@ -49,7 +52,7 @@ object NetworkModule {
             .baseUrl(BASE_URL)
             .client(okHttpClient)
             //json 변화기 Factory
-            .client(createOkHttpClient())
+            .client(provideHttpClient())
             .addConverterFactory(gsonConverterFactory)
             .build()
 
@@ -69,16 +72,12 @@ object NetworkModule {
         Log.d(ContentValues.TAG, "provideApiService: ")
         return retrofit.create(ApiInterface::class.java)
     }
+    
+    // 서버로 부터 받아온 데이터 log 찍기
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
 
 
-     private fun createOkHttpClient(): OkHttpClient {
-         Log.d(TAG, "createOkHttpClient: ")
-        val builder = OkHttpClient.Builder()
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        builder.addInterceptor(interceptor)
-        return builder.build()
-    }
 
 
 }
