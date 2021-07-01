de619bb537551a3ba882c6f975905deac3b8a220
KyungMin <2004ckck@naver.com>
2021-07-01 16:34:32 +0900

    [DELETE] api 변경후 아직 안쓰는 코드 제거

--- app/src/main/java/com/example/movie/data/RemoteDataSource.kt
+++ app/src/main/java/com/example/movie/data/RemoteDataSource.kt
@@ -1,41 +1,18 @@
 package com.example.movie.data
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
+import com.example.movie.data.network.ApiInterface
 import com.example.movie.models.Movie
-import com.example.movie.network.ApiInterface
-import com.example.movie.untils.Constants.Companion.TAG
 import retrofit2.Response
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val apiInterface: ApiInterface
-
 ) {
 
-    suspend fun getPopularMovie(queries: Map<String, String>, page: Int): Response<Movie> {
-        Log.d(TAG, "RemoteDataSource - getPopularMovie() called")
-        return apiInterface.getPopularMovie(queries, page)
-    }
 
-    suspend fun getDiscoverMovie(queries: Map<String, String>, page: Int): Response<Movie> {
-        Log.d(TAG, "RemoteDataSource - getDiscoverMovie() called")
-        return apiInterface.getDiscoverMovies(queries, page)
-    }
+    suspend fun getMovie(page:Int): Response<Movie> {
 
-    suspend fun getRecentMovie(queries: Map<String, String>, page: Int): Response<Movie> {
-        Log.d(TAG, "RemoteDataSource - getRecentMovie() called")
-        return apiInterface.getRecentMovie(queries, page)
-    }
-
-    suspend fun getTopMovie(queries: Map<String, String>, page: Int): Response<Movie> {
-        Log.d(TAG, "RemoteDataSource - getTopMovie() called")
-        return apiInterface.getTopRatedMovies(queries, page)
-    }
-
-    suspend fun getMovieList(queries: Map<String, String>, page: Int): Response<Movie> {
-        Log.d(TAG, "RemoteDataSource - getTopMovie() called")
-        return apiInterface.getMovieList(queries, page)
+        return apiInterface.getMovie(page)
     }
 
 }
